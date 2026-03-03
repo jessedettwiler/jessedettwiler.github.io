b2945d5063742d1d430d52590ebcaf17af3c5430
jessedettwiler <jessedettwiler@gmail.com>
2026-03-03 10:10:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -133,12 +133,8 @@ document.querySelectorAll("[data-slider]").forEach((slider) => {
   }, { passive: true });
 
   // Auto-resize
-if (typeof ResizeObserver !== "undefined") {
-  const ro = new ResizeObserver(measure);
-  ro.observe(viewport);
-} else {
-  window.addEventListener("resize", measure);
-}
+const ro = new ResizeObserver(measure);
+ro.observe(viewport);
 
   // -------------------------
   // AUTOPLAY (soft + safe)
